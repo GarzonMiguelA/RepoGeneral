0a7032c968ca92949a96d2a680c596dd56b84146
GarzonMiguelA <miguel.garzon7.7@itb.cat>
2024-12-13 11:37:28 +0100

    commit

---
+++ ListaTareasInteractiva/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import TareaItem from './components/TareaItem.vue';
+  import { ref, computed } from 'vue';
+
+  
+  //Creamos la variable para guardar tarea
+  const tareas = ref([]);
+  const nuevaTarea = ref('');
+
+
+  const guardadoDeTareas = () => {
+    if(nuevaTarea.value.trim() !== ''){
+      tareas.value.push(nuevaTarea.value)
+      nuevaTarea.value = ''
+    }
+  }; 
+</script>
+
+<template>
+   <h1>Bienvenido a la lista de tareas interactivas!</h1>
+   <form @submit.prevent="guardadoDeTareas">
+      <label>Escribe las tareas</label>
+      <input type="text" v-model="nuevaTarea">
+      <button type="submit">Añadir</button>
+   </form>
+   <h2>Lista de tareas añadidas</h2>
+   <TareaItem :tareas="tareas"/>
+</template>
+
+
---
+++ ListaTareasInteractiva/src/components/TareaItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { ref } from 'vue';
+  import { defineProps, defineEmits } from 'vue';
+
+  const props =defineProps({
+    tareas: {
+      type: Array,
+      required: true
+    }    
+  })
+
+</script>
+
+<template>
+  <ul>
+    <li v-for="(tarea, index) in tareas" :key="index">
+      <TareaItem :tarea="tarea" />
+        {{ tarea }}
+    </li>
+  </ul>
+
+</template>
+
+
